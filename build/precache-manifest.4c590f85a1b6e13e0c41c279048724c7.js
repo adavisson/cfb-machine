2831aba2dba922b0d60d4438abc762940f05a6ff
Andrew Davisson <akdavisson4@gmail.com>
2019-12-09 10:38:56 -0700

    new build

---
+++ build/precache-manifest.4c590f85a1b6e13e0c41c279048724c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380d084cf69f7f0d68b8bb14a667ad0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdc8de39245db5d0ddcf",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0e702deaeac6dc963760",
+    "url": "/static/css/main.434f09f4.chunk.css"
+  },
+  {
+    "revision": "cdc8de39245db5d0ddcf",
+    "url": "/static/js/2.7ab745db.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.7ab745db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0e702deaeac6dc963760",
+    "url": "/static/js/main.399343fc.chunk.js"
+  },
+  {
+    "revision": "390913b2f85b9639fbdf",
+    "url": "/static/js/runtime-main.78d9dc22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
