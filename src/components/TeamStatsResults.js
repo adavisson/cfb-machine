d892545bf89cb2412136a7b9a45c5fafe20dfa64
Andrew Davisson <akdavisson4@gmail.com>
2019-12-10 22:54:32 -0700

    fixed styling for stats results

--- src/components/TeamStatsResults.js
+++ src/components/TeamStatsResults.js
@@ -15,7 +15,7 @@ const TeamStatsResults = (props) => {
   },[])
 
   return (
-    <div className="team-stats-results page">
+    <div className="team-stats-results">
       <h3>{props.year} Stats for {props.team}</h3>
       {!!loaded && stats.map(stat => {
         return(<p>{stat.statName}: {stat.statValue}</p>);
