9de2378fa4a916f18db77d0ee5f4b5fe055a3bff
Andrew Davisson <akdavisson4@gmail.com>
2020-08-18 09:06:53 -0600

    added try catch block

--- src/actions/fetchTeams.js
+++ src/actions/fetchTeams.js
@@ -1,8 +1,12 @@
 export const fetchTeams = () => {
   return async (dispatch) => {
     dispatch({ type: 'LOADING_TEAMS' })
-    const result = await fetch('https://api.collegefootballdata.com/teams/fbs')
-    const teams = await result.json()
-    dispatch({ type: 'ADD_TEAMS', teams: teams})
+    try {
+      const result = await fetch('https://api.collegefootballdata.com/teams/fbs')
+      const teams = await result.json()
+      dispatch({ type: 'ADD_TEAMS', teams: teams})
+    } catch(e) {
+      console.log(e)
+    }
   }
 }
