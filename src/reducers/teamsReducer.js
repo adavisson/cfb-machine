97b8444f38de79ab1d6beacebaa0ec26b3ecf96c
Andrew Davisson <akdavisson4@gmail.com>
2020-08-18 08:28:24 -0600

    built out reducer

---
+++ src/reducers/teamsReducer.js
@@ -0,0 +1,19 @@
+const teamsReducer = (state = {teams: [], loading: false}, action){
+  switch(action.type){
+    case 'LOADING_TEAMS':
+      return {
+        ...state,
+        loading: true
+      }
+    case 'ADD_TEAMS':
+      return {
+        ...state,
+        teams: [action.teams],
+        loading: false
+      }
+    default: 
+      return state
+  }
+}
+
+export default teamsReducer
